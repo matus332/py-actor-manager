a9c8cd7f5ff403cf3fdb6cca9d850f602b5efddb
Denys Matusevych <denytop3@gmail.com>
2026-04-18 23:38:26 +0300

    fix solution

--- app/managers.py
+++ app/managers.py
@@ -10,8 +10,8 @@ class ActorManager:
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name)"
-            f"VALUES (?, ?) ",
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
+            "VALUES (?, ?)",
             (first_name, last_name)
         )
         self._connection.commit()
